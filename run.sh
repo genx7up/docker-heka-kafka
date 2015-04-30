b7677b680875e35c900b081c87f9baeb288c864e
genx7up <genx7up@users.noreply.github.com>
2015-04-29 19:35:22 -0700

    bump to 0.9.2, removed external kafka

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 git pull
-git checkout v0.7.2
+git checkout v0.9.2
 source build.sh
 
 #Install Kafka Plugin
 cd /opt/heka
-echo 'add_external_plugin(git https://github.com/genx7up/heka-kafka acf3ac7a3d6d6dab313510f81828fca2f9375229)' >> cmake/plugin_loader.cmake
-go get github.com/Shopify/sarama
+echo 'add_external_plugin(git https://github.com/genx7up/heka-kafka ffdae590cb5f611aece01d27dbe5cfd2632229bf)' >> cmake/plugin_loader.cmake
 source build.sh
 
 #Copy the new binary to shared volume
-cp heka/bin/hekad /data
+cp build/heka/bin/hekad /data
