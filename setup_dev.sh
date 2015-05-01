ff2b6c945e9777031c2967a09582b4beebade342
genx7up <genx7up@users.noreply.github.com>
2015-05-01 15:59:54 -0700

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -3,6 +3,6 @@ mkdir -p externals/heka-kafka
 cp -r build/heka/src/github.com/genx7up/heka-kafka/* externals/heka-kafka/
 echo 'add_external_plugin(git https://github.com/genx7up/heka-kafka :local)' > cmake/plugin_loader.cmake
 
-echo 'cd /opt/heka && source build.sh && rm -rf /var/cache/hekad/logstreamer/LogstreamerInput && hekad --config=/opt/heka/hekad.toml' > rebuild.sh
+echo 'cd /opt/heka && source build.sh && rm -rf /var/cache/hekad/logstreamer/LogstreamerInput && /opt/heka/build/heka/bin/hekad --config=/opt/heka/hekad.toml' > rebuild.sh
 chmod +x rebuild.sh
 ./rebuild.sh
