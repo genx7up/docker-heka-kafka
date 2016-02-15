7148f1cb424b9ca5ee725808350ec8413fa4edcb
genx7up <genx7up@users.noreply.github.com>
2016-02-15 15:16:52 -0800

    Upgrading to current dev v0.10.0 ++

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ source build.sh
 #Install Kafka Plugin
 cd /opt/heka
 go get github.com/linkedin/goavro
-echo 'add_external_plugin(git https://github.com/genx7up/heka-kafka 7cbdd308fc4e7875603ee7725b33b116f0131017)' >> cmake/plugin_loader.cmake
+echo 'add_external_plugin(git https://github.com/genx7up/heka-kafka aec1391b52185ca44cc728219b3629dca67b062c)' >> cmake/plugin_loader.cmake
 source build.sh
 
 #Copy the new binary to shared volume
